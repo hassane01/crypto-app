79be122db750de84daa96a7fe4e84e1258d86c97
hassane <oubakassehassane7@gmail.com>
2024-10-19 09:09:22 +0100

    fixing the issues with the search

--- src/components/Serach.js
+++ src/components/Serach.js
@@ -3,15 +3,21 @@ import '../styles/Search.css'
 import { ContextCrypto } from '../Helpers/Context'
 
 const Serach = () => {
-    const { CryptoDataV2 , setCryptoDataV2} = useContext(ContextCrypto)
-    const handlechange = (name)=>{
-     
-        const query = name.toLowerCase()
-        const Newarray = 
-        CryptoDataV2.filter(data=>data.name.toLowerCase().includes(query) )
-        setCryptoDataV2(Newarray)
-        
-    }
+    const { CryptoDataV1 , setCryptoDataV2} = useContext(ContextCrypto)
+    const handlechange = (name) => {
+      const query = name.toLowerCase();
+
+      // If search input is empty, restore the original data
+      if (query === '') {
+          setCryptoDataV2(CryptoDataV1);
+      } else {
+          // Filter the array based on the search query
+          const newArray = CryptoDataV1.filter(data =>
+              data.name.toLowerCase().includes(query)
+          );
+          setCryptoDataV2(newArray);
+      }
+  };
   return (
     <div className='search'>
         <h1>Crypto Tracker</h1>
